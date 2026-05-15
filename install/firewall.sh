8a47dc175ccb00fab9778f1ce5fc25b8ce1e1797
Kelly McCuddy <kmccuddy@startmail.com>
2026-05-15 19:50:09 -0400

    changed services to service

--- install/firewall.sh
+++ install/firewall.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 sudo xbps-install -Sy ufw
 
 echo "Enabling and starting ufw."
-sudo ln -s /etc/sv/ufw /var/services/
+sudo ln -s /etc/sv/ufw /var/service/
 sudo ufw default deny incoming
 sudo ufw default allow outgoing
 sudo ufw enable
