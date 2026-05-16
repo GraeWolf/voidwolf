c4ad73207cad567123521ac12a8ebdc4ae5c6f48
Kelly McCuddy <kmccuddy@startmail.com>
2026-05-16 13:35:29 -0400

    initial commit

---
+++ install/tokyonight_theme.sh
@@ -0,0 +1,35 @@
+#/usr/bin/env bash
+
+set -euo pipefail
+
+if xbps-query -s git &>/dev/null; then
+  echo "git is installed"
+else
+  echo "git is not installed."
+  echo "installing now."
+  sudo xbps-install -Sy git
+fi
+
+if xbps-query -s sassc &>/dev/null; then
+  echo "sassc requirement met"
+else
+  echo "sassc not installed"
+  echo "installing sassc"
+  sudo xbps-install -Sy sassc
+fi 
+
+if [ -d "$HOME/.local/share/themes" ]; then
+  echo "themes directory exists"
+else
+  echo "creating themes directory"
+  mkdir -p $HOME/.local/share/themes
+fi
+
+git clone https://github.com/Fausto-Korpsvart/Tokyonight-GTK-Theme.git
+
+cd Tokyo-Night-GTK-Theme/themes
+./install.sh -d $HOME/.local/share/themes --tweaks black macos -t purple -c dark -l 
+
+
+
+
