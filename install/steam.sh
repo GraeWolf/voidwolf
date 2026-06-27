4b68c5a66f41a513a803e054922a0cdaa72ce92b
Graewolf <gitEmail>
2026-06-27 17:07:31 -0400

    added more packages needed for steam to operate.

--- install/steam.sh
+++ install/steam.sh
@@ -2,5 +2,5 @@
 
 set -euo pipefail
 
-sudo xbps-install -Sy steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit libva-32bit
+sudo xbps-install -Sy steam libgcc-32bit libstdc++-32bit libdrm-32bit libglvnd-32bit libva-32bit nvidia580-libs-32bit vulkan-loader-32bit
 setsid gtk-launch steam >/dev/nll 2>&1 &
