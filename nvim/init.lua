44d961607f2d447cc6c38f7799b08b482dc72515
Graewolf <gitEmail>
2026-07-11 14:49:44 -0400

    remake of nvim config

---
+++ nvim/init.lua
@@ -0,0 +1,10 @@
+vim.opt.termguicolors = true
+require("options")
+require("keymaps")
+require("commands")
+require("pack")
+require("treesitter")
+require("lsp")
+
+vim.cmd.colorscheme("tokyonight-night")
+
